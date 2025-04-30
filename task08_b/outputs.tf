077f84774bd3d04e62d49535a20f2d66919bd809
Shashwat Swaraj <shashwat_swaraj@epam.com>
2025-04-30 15:51:22 +0530

    2

--- task08_b/outputs.tf
+++ task08_b/outputs.tf
@@ -1,11 +1,14 @@
 output "redis_fqdn" {
-  value = module.aci_redis.redis_fqdn
+  description = "FQDN of Redis in Azure Container Instance"
+  value       = module.aci_redis.redis_fqdn
 }
 
 output "aca_fqdn" {
-  value = module.aca.aca_fqdn
+  description = "FQDN of App in Azure Container App"
+  value       = module.aca.aca_fqdn
 }
 
 output "aks_lb_ip" {
-  value = module.k8s.lb_ip
+  description = "Load Balancer IP address of APP in AKS"
+  value       = module.k8s.lb_ip
 }
--- task08_b/terraform.tfvars
+++ task08_b/terraform.tfvars
@@ -1,3 +1,4 @@
+name_prefix            = "cmtr-57d8b090-mod8b"
 location               = "eastus"
 tenant_id              = "<your-tenant-id>"
 current_user_object_id = "<your-object-id>"
